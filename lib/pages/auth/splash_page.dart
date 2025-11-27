8a041605f236f2005dfedf047e23b82b68c0c1ec
donyradjah <donyradjah@gmail.com>
2025-11-27 17:09:46 +0700

    refactor(service): pindahkan InformasiService ke folder guest

--- lib/pages/auth/splash_page.dart
+++ lib/pages/auth/splash_page.dart
@@ -3,7 +3,7 @@ import 'package:dmpku/core/themes/app_text_styles.dart';
 import 'package:dmpku/core/themes/theme_extension.dart';
 import 'package:dmpku/gen/assets.gen.dart';
 import 'package:dmpku/pages/guest/main_page.dart';
-import 'package:dmpku/service/informasi_service.dart';
+import 'package:dmpku/service/guest/informasi_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
