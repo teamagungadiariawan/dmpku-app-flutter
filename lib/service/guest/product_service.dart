ba801c3e64a9aa8a43f77a83e1a640425003394f
donyradjah <donyradjah@gmail.com>
2025-11-28 17:12:37 +0700

    feat(service): rename produk_service dan tambah getPulsaGuestProducts

--- lib/service/guest/product_service.dart
+++ lib/service/guest/product_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:dmpku/core/apiconfig/api_client_guest.dart';
 import 'package:dmpku/core/apiconfig/base_response.dart';
 import 'package:dmpku/core/apiconfig/server_exception.dart';
+import 'package:dmpku/model/product_response.dart';
 import 'package:dmpku/model/provider_response.dart';
 import 'package:flutter/material.dart';
 
@@ -23,12 +24,35 @@ class ProdukService {
 
       return result;
     } on DioException catch (e, stackTrace) {
-
       debugPrintStack(stackTrace: stackTrace);
       debugPrint("DIO EXCEPTION PRODUK SERVICE: $e");
       throw ServerException.fromDio(e: e);
     }
   }
 
+  Future<BaseResponse<ListProductResponse>> getPulsaGuestProducts({
+    required int idProvider,
+  }) async {
+    try {
+      final response = await _dio.post(
+        "guest/pulsa/product",
+        data: {'idprovider': idProvider} as Map<dynamic, dynamic>,
+      );
 
+      final result = BaseResponse<ListProductResponse>.fromJson(
+        response.data,
+        fromJsonT: (json) => ListProductResponse.fromJson(json),
+      );
+
+      if (!result.status) {
+        throw ServerException.fromDio(r: response);
+      }
+
+      return result;
+    } on DioException catch (e, stackTrace) {
+      debugPrintStack(stackTrace: stackTrace);
+      debugPrint("DIO EXCEPTION PRODUK SERVICE: $e");
+      throw ServerException.fromDio(e: e);
+    }
+  }
 }
