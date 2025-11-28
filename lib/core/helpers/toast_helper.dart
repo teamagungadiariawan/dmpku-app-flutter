682bd51dab7e777c1f79a1d0d695bea1ab3d4728
donyradjah <donyradjah@gmail.com>
2025-11-28 13:27:32 +0700

    feat(core): tambah ApiStatus enum dan toast_helper

---
+++ lib/core/helpers/toast_helper.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:toastification/toastification.dart';
+
+void showSuccessMessage(String message, {
+  String title = "Sukses"
+}){
+  _showMessage(title: title, message: message, type: ToastificationType.success);
+}
+
+void showErrorMessage(String message, {
+  String title = "Terjadi Kesalahan"
+}){
+  _showMessage(title: title, message: message, type: ToastificationType.error);
+
+}
+
+void showWarningMessage(String message, {
+  String title = "Perhatian"
+}){
+  _showMessage(title: title, message: message, type: ToastificationType.warning);
+}
+
+void _showMessage({
+  required String title,
+  required String message,
+  required ToastificationType type
+}){
+  toastification.show(
+    type: type,
+    style: ToastificationStyle.flat,
+    title: Text(title),
+    description: Text(message),
+    alignment: Alignment.topCenter,
+    autoCloseDuration: const Duration(seconds: 4),
+    boxShadow: highModeShadow,
+    showProgressBar: true,
+    dragToClose: true,
+  );
+}
