d132499aab9f2d53b7c5d858239e676159dc56ce
donyradjah <donyradjah@gmail.com>
2025-11-28 16:14:58 +0700

    feat(theme): tambah app_spacing.dart untuk konstanta padding

---
+++ lib/core/themes/app_spacing.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const paddingCard = EdgeInsets.symmetric(horizontal: 10, vertical: 6);
+const paddingPage = EdgeInsets.symmetric(horizontal: 16, vertical: 6);
