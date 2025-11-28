cb1b49ad9188f7e64855dbac00922adb71e9856b
donyradjah <donyradjah@gmail.com>
2025-11-28 13:28:03 +0700

    feat(service): tambah ProdukService untuk fetch data provider pulsaTo run code, enable code execution and file creation in Settings > Capabilities.

---
+++ lib/service/guest/produk_service.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:dmpku/core/apiconfig/api_client_guest.dart';
+import 'package:dmpku/core/apiconfig/base_response.dart';
+import 'package:dmpku/core/apiconfig/server_exception.dart';
+import 'package:dmpku/model/provider_response.dart';
+import 'package:flutter/material.dart';
+
+class ProdukService {
+  final _dio = ApiClientGuest.dio;
+
+  Future<BaseResponse<ListProviderResponse>> getPulsaGuestProviders() async {
+    try {
+      final response = await _dio.post("guest/pulsa/provider", data: {});
+
+      final result = BaseResponse<ListProviderResponse>.fromJson(
+        response.data,
+        fromJsonT: (json) => ListProviderResponse.fromJson(json),
+      );
+
+      if (!result.status) {
+        throw ServerException.fromDio(r: response);
+      }
+
+      return result;
+    } on DioException catch (e, stackTrace) {
+
+      debugPrintStack(stackTrace: stackTrace);
+      debugPrint("DIO EXCEPTION PRODUK SERVICE: $e");
+      throw ServerException.fromDio(e: e);
+    }
+  }
+}
