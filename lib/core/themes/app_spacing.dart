bc843149beba0ca28467a928e9121378cab11ef6
donyradjah <donyradjah@gmail.com>
2025-11-29 13:24:38 +0700

    refactor(theme): tambah konstanta padding terpisah untuk page

--- lib/core/themes/app_spacing.dart
+++ lib/core/themes/app_spacing.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
 
 const paddingCard = EdgeInsets.symmetric(horizontal: 10, vertical: 6);
-const paddingPage = EdgeInsets.symmetric(horizontal: 16, vertical: 6);
+const paddinPageh = 16.0;
+const paddingPagev = 6.0;
+const paddingPage = EdgeInsets.symmetric(
+  horizontal: paddinPageh,
+  vertical: paddingPagev,
+);
