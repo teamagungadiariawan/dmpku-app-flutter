39032f83758689c216ab3f924573476d7d8e0e05
donyradjah <donyradjah@gmail.com>
2025-12-02 15:07:23 +0700

    refactor(aktivasi-voucher): hapus import yang tidak digunakan dari halaman provider
    - Hapus import app_text_styles yang tidak digunakan
    - Hapus import assets.gen yang tidak digunakan
    - Hapus import card_input_tujuan_pulsa yang tidak digunakan
    - Hapus import custom_popup_input_tujuan yang tidak digunakan
    - Hapus import button_favorit yang tidak digunakan
    - Hapus import paket_data yang tidak digunakan
    - Hapus import lottie yang tidak digunakan
    - Bersihkan statement import untuk maintainability yang lebih baik

--- lib/pages/guest/produk/isiulang/aktivasi_voucher/guest_aktivasi_voucher_provider_page.dart
+++ lib/pages/guest/produk/isiulang/aktivasi_voucher/guest_aktivasi_voucher_provider_page.dart
@@ -2,18 +2,11 @@ import 'package:dmpku/core/enums/api_status.dart';
 import 'package:dmpku/core/helpers/navigator_helper.dart';
 import 'package:dmpku/core/helpers/system_ui_helper.dart';
 import 'package:dmpku/core/themes/app_spacing.dart';
-import 'package:dmpku/core/themes/app_text_styles.dart';
 import 'package:dmpku/core/themes/theme_extension.dart';
-import 'package:dmpku/gen/assets.gen.dart';
 import 'package:dmpku/model/provider_response.dart';
 import 'package:dmpku/pages/guest/produk/isiulang/aktivasi_voucher/guest_aktivasi_voucher_produk_page.dart';
 import 'package:dmpku/pages/guest/produk/isiulang/aktivasi_voucher/aktivasi_voucher_provider.dart';
-import 'package:dmpku/pages/guest/produk/isiulang/paket_data/guest_paket_data_produk_page.dart';
-import 'package:dmpku/pages/guest/produk/isiulang/paket_data/paket_data_provider.dart';
-import 'package:dmpku/pages/guest/produk/isiulang/widgets/card_input_tujuan_pulsa.dart';
 import 'package:dmpku/widgets/custom_app_bar.dart';
-import 'package:dmpku/widgets/produk/custom_popup_input_tujuan.dart';
-import 'package:dmpku/widgets/produk/button_favorit.dart';
 import 'package:dmpku/widgets/produk/card_provider.dart';
 import 'package:dmpku/widgets/produk/card_provider_shimmer.dart';
 import 'package:dmpku/widgets/produk/refreshable_list.dart';
@@ -23,7 +16,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 import 'package:gap/gap.dart';
-import 'package:lottie/lottie.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class GuestAktivasiVoucherProviderPage extends StatefulWidget {
