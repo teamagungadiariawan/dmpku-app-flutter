3998368402d89e82ebeb74677b2016d97b17e48e
donyradjah <donyradjah@gmail.com>
2025-12-03 14:03:06 +0700

    refactor(guest-pages): migrasi halaman cek voucher, info kartu, aktivasi voucher
    - Ganti CardInputTujuanCekVoucher dengan CardInputTujuan
    - Tambah addButtonLanjutkan=true untuk halaman yang butuh tombol lanjutkan
    - Cek status voucher: ubah label ke 'Kode Voucher', icon ke MdiIcons.ticket
    - Info kartu: aktifkan tombol lanjutkan dengan addButtonLanjutkan
    - Hapus import yang tidak terpakai

--- lib/pages/guest/produk/isiulang/cek_status_voucher/guest_cek_status_voucher_produk_page.dart
+++ lib/pages/guest/produk/isiulang/cek_status_voucher/guest_cek_status_voucher_produk_page.dart
@@ -1,24 +1,17 @@
-import 'package:dmpku/core/enums/api_status.dart';
 import 'package:dmpku/core/helpers/navigator_helper.dart';
 import 'package:dmpku/core/helpers/system_ui_helper.dart';
 import 'package:dmpku/core/themes/app_spacing.dart';
-import 'package:dmpku/model/product_response.dart';
 import 'package:dmpku/pages/guest/produk/isiulang/cek_status_voucher/cek_status_voucher_provider.dart';
-import 'package:dmpku/pages/guest/produk/isiulang/widgets/card_input_tujuan_cek_voucher.dart';
-import 'package:dmpku/pages/guest/produk/isiulang/widgets/card_input_tujuan_pulsa.dart';
+import 'package:dmpku/pages/guest/produk/isiulang/widgets/card_input_tujuan.dart';
 import 'package:dmpku/widgets/custom_app_bar.dart';
 import 'package:dmpku/widgets/dialog/belum_login_dialog.dart';
-import 'package:dmpku/widgets/produk/button_checkout.dart';
-import 'package:dmpku/widgets/produk/card_product_pulsa.dart';
-import 'package:dmpku/widgets/produk/card_product_pulsa_shimmer.dart';
 import 'package:dmpku/widgets/produk/card_provider.dart';
 import 'package:dmpku/widgets/produk/custom_popup_input_tujuan.dart';
-import 'package:dmpku/widgets/produk/refreshable_list.dart';
-import 'package:dmpku/widgets/produk/sort_filter_product.dart';
 import 'package:dmpku/widgets/shake_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 
 class GuestCekStatusVoucherProdukPage extends StatefulWidget {
   static const routeName = '/guest/produk/isiulang/cek-status-voucher/produk';
@@ -76,13 +69,13 @@ class _GuestCekStatusVoucherProdukPageState
           previous.tujuan != current.tujuan ||
           previous.tujuanHasError != current.tujuanHasError,
       builder: (context, state) {
-        return CardInputTujuanCekVoucher(
+        return CardInputTujuan(
           tujuan: state.tujuan,
-          label: 'No. Tujuan',
+          label: 'Kode Voucher',
           hasError: state.tujuanHasError,
           errorMessage: state.tujuanErrorMessage,
           isEditable: true,
-          hintText: 'Masukkan No. Tujuan',
+          hintText: 'Contoh : 1234XXXXXXXXXXX',
           controller: state.tujuanController,
           focusNode: state.tujuanFocusNode,
           onChanged: (value) {
@@ -96,6 +89,7 @@ class _GuestCekStatusVoucherProdukPageState
           shakeKey: shakeKey,
           showFavoritButton: true,
           isGuest: true,
+          addButtonLanjutkan: true,
           onLanjutkan: () {
             var valid = getCekStatusVoucherProvider(context).validateTujuan();
             if (!valid) {
@@ -111,6 +105,7 @@ class _GuestCekStatusVoucherProdukPageState
               context,
             ).setTujuan(val, updateController: true);
           },
+          icon: MdiIcons.ticket,
           suffixWidget: CustomPopupInputTujuan(
             isContact: true,
             isVoice: true,
