e6ee6a119c0d7281ef267ffbf6e1b629dd10b573
donyradjah <donyradjah@gmail.com>
2025-12-04 15:58:25 +0700

    feat(helper): tambah fungsi obfuscatePhone untuk samarkan nomor
    - Fungsi untuk menyembunyikan sebagian digit nomor telepon
    - Parameter customizable untuk jumlah digit yang ditampilkan

--- lib/core/helpers/strings_helper.dart
+++ lib/core/helpers/strings_helper.dart
@@ -1,8 +1,27 @@
-String ToRupiah(String amount)  {
+String ToRupiah(String amount) {
   try {
     final number = int.parse(amount.replaceAll(RegExp(r'[^0-9]'), ''));
     return 'Rp ${number.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]}.')}';
   } catch (e) {
     return amount;
   }
+}
+
+String obfuscatePhone(
+  String phone, {
+  int visibleStart = 3,
+  int visibleEnd = 3,
+}) {
+  // Hapus karakter non-digit
+  final digits = phone.replaceAll(RegExp(r'\D'), '');
+
+  if (digits.length <= visibleStart + visibleEnd) {
+    return phone;
+  }
+
+  final start = digits.substring(0, visibleStart);
+  final end = digits.substring(digits.length - visibleEnd);
+  final hiddenLength = digits.length - visibleStart - visibleEnd;
+
+  return '$start${'*' * hiddenLength}$end';
 }
